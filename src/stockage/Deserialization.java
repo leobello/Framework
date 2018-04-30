0bcd254c4ef9675ff964207a765418c200b0dad1
MouatazAmal <mouataz.amal@gmail.com>
2018-04-30 17:33:25 +0200

    Serialization et deserialization

---
+++ src/stockage/Deserialization.java
@@ -0,0 +1,23 @@
+package stockage;
+import java.io.*;
+
+
+public class Deserialization implements Serializable {
+
+	public File f;
+	public Object ReadedObject;
+	
+		public Deserialization(String fichier) throws FileNotFoundException, IOException, ClassNotFoundException{
+			this.f= new File (fichier);
+			ObjectInputStream ois =  new ObjectInputStream(new FileInputStream(fichier)) ;
+			this.ReadedObject = ois.readObject();
+		}
+		
+		public Object ObjectLu() {
+			return this.ReadedObject;
+		}
+		
+		public String FileName(){
+			return f.getName();
+		}
+}
---
+++ src/stockage/Serialization.java
@@ -0,0 +1,18 @@
+package stockage;
+
+import java.io.*;
+
+public class Serialization implements Serializable {
+
+	public File f;
+	
+	public Serialization(String fichier, Object o) throws FileNotFoundException, IOException{
+		this.f= new File (fichier);
+		ObjectOutputStream oos =  new ObjectOutputStream(new FileOutputStream(fichier)) ;
+		oos.writeObject(o);
+	}
+	
+	public String FileName(){
+		return f.getName();
+	}
+}
