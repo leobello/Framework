1ee7fe18a0118d4b05dcef30bf74ad8b13a9d49b
amine <aminebouz84@gmail.com>
2018-05-02 11:12:24 +0200

    je viens d'ajouter des truc

--- src/serveur/Test.java
+++ src/serveur/Test.java
@@ -1,13 +1,27 @@
 package serveur;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 public class Test {
-	public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, IOException {
-		Inscription isc=new Inscription();
+	public static void main(String[] args) throws RemoteException, FileNotFoundException, ClassNotFoundException, IOException {
+		try {
+			LocateRegistry.createRegistry(1145);
+		   /* if (System.getSecurityManager() == null) {
+		      System.setSecurityManager(new RMISecurityManager());
+		    }*/
+		    Actor serv=new Actor();
+		    String url = "rmi://localhost/Connect";
+		    System.out.println("Enregistrement de l'objet avec l'url : " + url);
+		    Naming.rebind(url, serv);
+		    System.out.println("Bind réaliser ");
+		  } catch (Exception e) {
+		     e.printStackTrace();
+		  }
+		/*Inscription isc=new Inscription();
 		isc.ajouter();
-		Actor usr=new Actor();
-		usr.connectUser();
+		UtilisateurServ usr=new UtilisateurServ();
+		usr.connectUser();*/
 	}
 }
