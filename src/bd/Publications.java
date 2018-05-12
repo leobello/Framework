1d47b1b9c4ea4e93f26a7be15c7252f84638869d
Your Name <you@example.com>
2018-05-12 18:26:22 +0200

    nouvelle fonctionnalité

--- src/bd/Publications.java
+++ src/bd/Publications.java
@@ -41,6 +41,27 @@ public class Publications extends UnicastRemoteObject  implements Serializable {
 		}
 	}
 	
+	public ArrayList<Contenu> LireFichierstocke(int nbfile){
+		File f = new File("Archive"+nbfile);
+		if (f.exists()) {
+			try {
+				Deserialization dsz = new Deserialization(f);
+				return (ArrayList<Contenu>) dsz.ObjectLu();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+		
+	}
+	
 	public void afficher_Publications() {
 		for (Contenu x : contenuPublique) {
 			System.out.println(x.getContenu());
