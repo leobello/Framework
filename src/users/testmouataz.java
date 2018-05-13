4cb9ec4535eb122417594b04def46dba2f17e451
Your Name <you@example.com>
2018-05-13 13:03:19 +0200

    test de publication pour zak

--- src/users/testmouataz.java
+++ src/users/testmouataz.java
@@ -16,22 +16,18 @@ public class testmouataz {
 	
 		
 		public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException{		
-			// test recherche d'utilisateur
+
 			User pers = new User("mouataz", 22);
 			
 			String url = "rmi://"+Serveur.listen+"/Gnaouas";
 			
 			_Publications  OpenSpace = (_Publications)Naming.lookup(url);
-			// j'arrive pas r�usssi � cr�er le contenu parce que je bloque sur le troisi�me argument public, est ce que tu peux g�rer ca stp
-			// sinon tout est bon il va falloir juste tester cette fonctionnalit�.
-
-			Contenu C = new Contenu(new Photo(""), pers, new Public());
+	
+			Contenu C = new Contenu(new Photo("MouatazPhotoTest.jpg"), pers, new Public());
 			
-			//pers.publier(c);
+			pers.publier(C);
 			
-			OpenSpace.afficher_Publications();
-			
-			
+			OpenSpace.afficher_Publications();			
 		}
 		
 
